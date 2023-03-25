eda610aae6b6b5ee0f3bd998f651865015d07ad7
Lucas-Christian <lucas.christian.programmer@gmail.com>
2023-03-25 19:53:19 -0300

    Updated

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
-const pokemonName = document.querySelector('.pokemon_name');
-const pokemonNumber = document.querySelector('.pokemon_number');
-const pokemonImage = document.querySelector('.pokemon_image');
+const pokemonName = document.querySelector(".pokemon_name");
+const pokemonNumber = document.querySelector(".pokemon_number");
+const pokemonImage = document.querySelector(".pokemon_image");
 
-const form = document.querySelector('.form');
-const input = document.querySelector('.input_search');
-const buttonPrev = document.querySelector('.btn-prev');
-const buttonNext = document.querySelector('.btn-next');
+const form = document.querySelector(".form");
+const input = document.querySelector(".input_search");
+const buttonPrev = document.querySelector(".btn-prev");
+const buttonNext = document.querySelector(".btn-next");
 
 let searchPokemon = 1;
 
@@ -20,38 +20,38 @@ const fetchPokemon = async (pokemon) => {
 
 const renderPokemon = async (pokemon) => {
 
-  pokemonName.innerHTML = 'Loading...';
-  pokemonNumber.innerHTML = '';
+  pokemonName.innerHTML = "Loading...";
+  pokemonNumber.innerHTML = "";
 
   const data = await fetchPokemon(pokemon);
 
   if (data) {
-    pokemonImage.style.display = 'block';
+    pokemonImage.style.display = "block";
     pokemonName.innerHTML = data.name;
     pokemonNumber.innerHTML = data.id;
-    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
-    input.value = '';
+    pokemonImage.src = data["sprites"]["versions"]["generation-v"]["black-white"]["animated"]["front_default"];
+    input.value = "";
     searchPokemon = data.id;
   } else {
-    pokemonImage.style.display = 'none';
-    pokemonName.innerHTML = 'Not found :c';
-    pokemonNumber.innerHTML = '';
+    pokemonImage.style.display = "none";
+    pokemonName.innerHTML = "Not found :c";
+    pokemonNumber.innerHTML = "";
   }
 }
 
-form.addEventListener('submit', (event) => {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
   renderPokemon(input.value.toLowerCase());
 });
 
-buttonPrev.addEventListener('click', () => {
+buttonPrev.addEventListener("click", () => {
   if (searchPokemon > 1) {
     searchPokemon -= 1;
     renderPokemon(searchPokemon);
   }
 });
 
-buttonNext.addEventListener('click', () => {
+buttonNext.addEventListener("click", () => {
   searchPokemon += 1;
   renderPokemon(searchPokemon);
 });
